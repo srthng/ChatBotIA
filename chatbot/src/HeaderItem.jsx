75a7808088dc342fca489e7c47c1079d60b0adaf
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-11 16:36:08 -0300

    sa

--- chatbot/src/HeaderItem.jsx
+++ chatbot/src/HeaderItem.jsx
@@ -1,11 +1,23 @@
 import {Link} from 'react-router-dom'
 
-export default function HeaderItem() {
+export default  function HeaderItem () {
     return (
-        <div>
-            <h1>Bem-vindo ao nosso site</h1>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="Chatbot">Chatbot</Link></li>
-        </div>
+        <header>
+            <div className="inicio">
+            <h1>Maria botânica</h1>
+            </div>
+
+            <div className="meio">
+            <div className="botaodirecional1"><Link to="/">Home</Link></div>
+            <div className="botaodirecional2"><Link to="Chatbot">Chatbot</Link></div>
+            <div className="botaodirecional2"><Link to="Chatbot">Marketplace</Link></div>
+            </div>
+   
+
+            <div className="fim">
+            <div className="botaodirecional2"><Link to="Chatbot">login</Link></div>
+            <div className="botaodirecional2"><Link to="Chatbot">registrar</Link></div>
+            </div>
+        </header>
     )
 }
